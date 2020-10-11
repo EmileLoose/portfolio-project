e9cc21e87c49cb0b2b4502a13b153376834b5e6c
Emile Loose <emile.loose@planet.nl>
2020-10-11 09:29:31 +0200

    New local setting

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -126,3 +126,12 @@ STATIC_ROOT = BASE_DIR/ 'static'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR/ 'media'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file is present")
+else:
+    pass
+finally:
+    pass
